d2a7ddf56b4a9927c012ca3e0219ee688577c254
Dominik Liebler <liebler.dominik@gmail.com>
2023-02-12 13:28:51 +0100

    fetch URL from JS

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -1,4 +1,5 @@
 use serde::Deserialize;
+use gloo::utils::window;
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 pub struct Project {
@@ -8,10 +9,20 @@ pub struct Project {
     pub color: String,
 }
 
+fn get_base_url() -> String {
+    let location = window().location();
+    
+    format!(
+        "{}//{}",
+        location.protocol().expect("Error getting protocol"),
+        location.host().expect("Error getting host"),
+    )
+}
+
 pub async fn get_projects() -> Result<Vec<Project>, reqwest::Error> {
     let client = reqwest::Client::new();
 
-    let resp = client.get("/api/projects")
+    let resp = client.get(format!("{}/api/projects", get_base_url()))
         .bearer_auth("c79f9e52-86eb-4de6-be78-a7a097b8f516")
         .send()
         .await;
